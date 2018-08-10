3f26bcafe19eecc9154c770c10ec00061620175c
TIE <riccardoarona>
2018-08-10 14:44:25 +0200

    python command in bash

--- Sensor_Hat/src/start.sh
+++ Sensor_Hat/src/start.sh
@@ -5,6 +5,8 @@ if [ "$INITSYSTEM" != "on" ]; then
   /usr/sbin/sshd -p 22 &
 fi
 
+python "/usr/src/app/src/thread_sense.py"
+
 echo "This is where your application would start..."
 while : ; do
   echo "waiting"
